8178df41ce0b2da12beaf29e0698b77749dbe4f2
yanggang <yangglive@icloud.com>
2021-12-27 15:12:42 +0800

    change 0.0.3

--- server/server.go
+++ server/server.go
@@ -264,8 +264,7 @@ func (s *Server) setupCart() error {
 			since := time.Now()
 			defer func() {
 				d := time.Since(since)
-				clientIp := GetClientIp(c.Request)
-				s.Logger.Printf(infoServerPrefix+"track %v %v %v \n", clientIp, c.Request.RequestURI, d)
+				s.Logger.Printf(infoServerPrefix+"track host:%v path:%v %v \n", c.Request.Host, c.Request.RequestURI, d)
 			}()
 			findHost := false
 			var defaultDoamin *sections.Domain
